44a1208f1358a21712d21d77d7e18346f8c8dbf4
Nicolas Messer <nme@nicolasmesser.ch>
2024-04-30 15:01:36 +0200

    Continue Structure Adaptations & Improvements

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const threePoints = document.getElementById("three-points");
-const onePoint = document.getElementById("one-point");
+/* HOME */
+const onePointHome = document.getElementById("1-pt-h");
+const twoPointsHome = document.getElementById("2-pts-h");
+const threePointsHome = document.getElementById("3-pts-h");
 const scoreHome = document.getElementById("score-home");
+
+/* GUEST */
+const onePointGuest = document.getElementById("1-pt-g");
+const twoPointsGuest = document.getElementById("2-pts-g");
+const threePointsGuest = document.getElementById("3-pts-g");
 const scoreGuest = document.getElementById("score-guest");
 
-console.log(threePoints.innerText);
-console.log(onePoint.innerText);
+console.log("HOME\n----");
+console.log(onePointHome.innerText);
+console.log(twoPointsHome.innerText);
+console.log(threePointsHome.innerText);
 console.log(scoreHome.innerText);
+console.log("\nGUEST\n-----");
+console.log(onePointGuest.innerText);
+console.log(twoPointsGuest.innerText);
+console.log(threePointsGuest.innerText);
 console.log(scoreGuest.innerText);
